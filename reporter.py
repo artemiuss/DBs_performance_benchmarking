8d9861bdc6d22614ddeb1bf45c2b73b74a040ec9
Artem Avetian <artemiustas@gmail.com>
2023-08-20 19:35:57 +0300

    chnages

--- reporter.py
+++ reporter.py
@@ -13,8 +13,7 @@ workload_list = [
     {"name":"Workload C: Read only", "alias":"workloadc"}
 ]
 
-#thread_list = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-thread_list = [1]
+thread_list = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
 for workload in workload_list:
     for stage in ["load", "run"]:

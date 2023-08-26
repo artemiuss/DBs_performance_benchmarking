739dd987bd76f2fbbe3d0c4df5de1c1c8309a45b
Artem Avetian <artemavetian@gmail.com>
2023-08-26 23:26:59 +0300

    changes

--- run_tests.sh
+++ run_tests.sh
@@ -7,9 +7,6 @@ OPERATIONCOUNT=1000
 declare -a WORKLOADS=("workloada" "workloadb" "workloadc")
 declare -a THREADS=(1 2 3 4 5 6 7 8)
 
-#declare -a WORKLOADS=("workloada")
-#declare -a THREADS=(6)
-
 YCSB_HOME="YCSB"
 
 for WORKLOAD in "${WORKLOADS[@]}"
